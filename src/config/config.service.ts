4128f8bc9f26adde2e3249a2eec82628a7726d96
Mingyang Li <mingyangli118@gmail.com>
2022-03-06 13:13:24 +1300

    Added comments

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: __dirname + '.env' });
 
+// I can't be bothered importing and configuring envs for every file that needs it,
+// So I made this secret obj variable so that any file can simply use this obj to access envs with a lot less code
 export const secrets = {
   REACT_APP_AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN,
   REACT_APP_AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID,
